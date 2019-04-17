ccd68944027ebc2a37d6bca1975c03cf44921d29
jialei <948962131@qq.com>
2019-04-17 18:45:37 +0800

    feat():加入百度统计

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,28 @@ import routes from './index.config'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
-  linkActiveClass: 'active',
+  linkActiveClass: 'active'
   // linkExactActiveClass: 'active'
 })
+
+// 百度统计
+router.afterEach((to, from, next) => {
+  setTimeout(() => {
+    var _hmt = _hmt || []
+    ;(function() {
+      document.getElementById('baidu_tj') &&
+        document.getElementById('baidu_tj').remove()
+      var hm = document.createElement('script')
+      hm.src = 'https://hm.baidu.com/hm.js?2bf062dd1aa6e1327d8274e19f65bc19'
+      hm.id = 'baidu_tj'
+      var s = document.getElementsByTagName('script')[0]
+      s.parentNode.insertBefore(hm, s)
+    })()
+  }, 0)
+})
+
+export default router
