c0bd9e73caf589d777eae50e59b485a2602a4107
jialei <948962131@qq.com>
2019-04-17 19:13:22 +0800

    fix():插入百度统计

--- src/router/index.js
+++ src/router/index.js
@@ -13,19 +13,11 @@ const router = new Router({
 })
 
 // 百度统计
-router.afterEach((to, from, next) => {
-  setTimeout(() => {
-    var _hmt = _hmt || []
-    ;(function() {
-      document.getElementById('baidu_tj') &&
-        document.getElementById('baidu_tj').remove()
-      var hm = document.createElement('script')
-      hm.src = 'https://hm.baidu.com/hm.js?2bf062dd1aa6e1327d8274e19f65bc19'
-      hm.id = 'baidu_tj'
-      var s = document.getElementsByTagName('script')[0]
-      s.parentNode.insertBefore(hm, s)
-    })()
-  }, 0)
+router.beforeEach((to, from, next) => {
+  if (to.path) {
+    _hmt.push(['_trackPageview', '/#' + to.fullPath])
+  }
+  next()
 })
 
 export default router
